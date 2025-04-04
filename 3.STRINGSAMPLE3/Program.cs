e9239897581e5e208e78f818c94f186e7d96a3d0
Wayne Wang <65530009+weijiewang001@users.noreply.github.com>
2025-04-04 15:35:20 +1100

    String Sample

---
+++ 3.STRINGSAMPLE3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string firstFriend = "John";
+        string scondFriend = "Wayne";
+        Console.WriteLine($"My friend are {firstFriend} and {scondFriend}!");
+    }
+}
