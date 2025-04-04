95095a7e8e8ab3c5d68250ff5993f5ab8eb60f1b
Wayne Wang <65530009+weijiewang001@users.noreply.github.com>
2025-04-04 15:45:08 +1100

    Trim string

---
+++ 4.TRIMSTRING4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string greeting = "                Hello World!                    ";
+        Console.WriteLine($"{greeting}");
+
+
+        string trimmedGreeting = greeting.TrimStart();
+        Console.WriteLine($"{trimmedGreeting}");
+
+        trimmedGreeting = greeting.TrimEnd();
+        Console.WriteLine($"{trimmedGreeting}");
+
+        trimmedGreeting = greeting.Trim();
+        Console.WriteLine($"{trimmedGreeting}");
+    }
+}
