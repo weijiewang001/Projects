b5e9a964f7035ce5704b37841ff97e0958b5fbad
Wayne Wang <65530009+weijiewang001@users.noreply.github.com>
2025-04-04 15:52:35 +1100

    Replace String

---
+++ 5.REPLACESTRING5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string sayHello = "Hello World!";
+        Console.WriteLine(sayHello);
+
+        sayHello = sayHello.Replace("Hello", "Greeting");
+        Console.WriteLine(sayHello);
+
+        Console.WriteLine(sayHello.ToUpper());
+        Console.WriteLine(sayHello.ToLower());
+    }
+}
