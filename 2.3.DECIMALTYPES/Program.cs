38cf9f465a1964554649dc9ce10c73b9bbe22a4c
wayne <weijiewang001@gmail.com>
2025-04-06 23:35:19 +1000

    Update number type
    Update number

---
+++ 2.3.DECIMALTYPES/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    decimal min = decimal.MinValue;
+    decimal max = decimal.MaxValue;
+    Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
+  }
+}
