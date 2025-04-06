e676127bc488801b9f59107423a8f44377640e11
wayne <weijiewang001@gmail.com>
2025-04-06 23:57:46 +1000

    NumberChallenge

---
+++ 2.4.NumberChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    double radius = 2.50;
+    double area = radius * radius * Math.PI;
+    Console.WriteLine("The answer is " + area);
+  }
+}
+
