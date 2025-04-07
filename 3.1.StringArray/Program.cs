99fb817d2fc5c94bc10edce37c7cd005fdf5de5e
wayne <weijiewang001@gmail.com>
2025-04-07 21:37:10 +1000

    multidimensional array

---
+++ 3.1.StringArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    Console.WriteLine(weekDays[0]);
+    Console.WriteLine(weekDays[1]);
+    Console.WriteLine(weekDays[2]);
+    Console.WriteLine(weekDays[3]);
+    Console.WriteLine(weekDays[4]);
+    Console.WriteLine(weekDays[5]);
+    Console.WriteLine(weekDays[6]);
+  }
+}
---
+++ 3.2.Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    int[,] array2DDeclaration = new int[4, 2];
+    int[,,] array3DDeclaration = new int[4, 2, 3];
+
+    int[,] array2DInitionloization = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+
+    // Three - dimensional array.
+
+    int[,,] array3D = new int[,,] { { { 1, 2, 3 }, { 4, 5, 6 } },
+                                    { { 7, 8, 9 }, { 10, 11, 12 } } };
+
+
+    Console.WriteLine(array2DInitionloization[0, 0]);
+    Console.WriteLine(array2DInitionloization[0, 1]);
+    Console.WriteLine(array2DInitionloization[1, 0]);
+    Console.WriteLine(array2DInitionloization[1, 1]);
+    Console.WriteLine(array2DInitionloization[3, 0]);
+    Console.WriteLine(array2DInitionloization[3, 1]);
+
+    Console.WriteLine(array3D[1, 0, 1]);
+    Console.WriteLine(array3D[1, 1, 2]);
+
+
+
+  }
+}
