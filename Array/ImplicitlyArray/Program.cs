7a03dfd967e2c0789fea52ee93a6979fd1134927
wayne <weijiewang001@gmail.com>
2025-04-07 22:52:32 +1000

    Implicitly array

---
+++ Array/ImplicitlyArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    int[] a = new int[] { 1, 10, 100, 1000 };
+
+    Console.WriteLine("First Element: " + a[0]);
+    Console.WriteLine("Second Element: " + a[1]);
+    a[1] = 30;
+    Console.WriteLine("Second 30 Element: " + a[1]);
+
+    Console.WriteLine("Third Element: " + a[2]);
+    Console.WriteLine("Fourth Element: " + a[3]);
+  }
+}
