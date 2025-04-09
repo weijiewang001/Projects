5012c6dff4b52380516da2de7c86461350638d06
wayne <weijiewang001@gmail.com>
2025-04-10 01:05:12 +1000

    Update class

---
+++ Class/1.OOP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Car
+{
+  // Attribute or fields
+  public string Make;
+  public string Model;
+  public int Year;
+
+  // Behaviours or methods
+  public void Start()
+  {
+    Console.WriteLine("The car is started.");
+  }
+  public void Drive()
+  {
+    Console.WriteLine("The car is in motion.");
+  }
+}
+class Program
+{
+  static void Main()
+  {
+    Car myCar = new Car();
+    myCar.Make = "Benz";
+    myCar.Model = "C200";
+    myCar.Year = 2022;
+
+
+    Car mySecondCar = new Car();
+    mySecondCar.Make = "Audi";
+    mySecondCar.Model = "Q7 55TFSI";
+    mySecondCar.Year = 2020;
+
+    Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}");
+    myCar.Start();
+    myCar.Drive();
+
+
+    Console.WriteLine($"My second car is a {mySecondCar.Year} {mySecondCar.Make} {mySecondCar.Model}");
+    myCar.Start();
+    myCar.Drive();
+  }
+}
