ddb7226aae41d65433facfa90f8b0c0fcb87bb2a
wayne <weijiewang001@gmail.com>
2025-04-10 23:04:52 +1000

    Constant

---
+++ Class/4.OOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Circle
+{
+  // Public field
+  private const double Pi = 3.14159;
+
+  private double radius;
+
+  public Circle(double radius)
+  {
+    this.radius = radius;
+  }
+
+  public double CalculateArea()
+  {
+    return Pi * radius * radius;
+  }
+}
+class Program
+{
+  static void Main()
+  {
+    Circle circle = new Circle(5.0);
+    Console.WriteLine($"Area: {circle.CalculateArea()}");
+  }
+}
