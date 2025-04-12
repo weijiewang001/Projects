7d88a793552e43572c023a54d305175e2e3a60b7
wayne <weijiewang001@gmail.com>
2025-04-13 05:19:12 +1000

    Events

---
+++ Class/7.Events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class EventPublisher
+{
+  public event EventHandler<string> MyEvent;
+
+  public void TriggerEvent(string message)
+  {
+    if (MyEvent != null)
+    {
+      MyEvent(this, message);
+    }
+  }
+}
+public class EventSubscriber
+{
+  public EventSubscriber(EventPublisher publisher)
+  {
+    publisher.MyEvent += HandleEvent;
+  }
+  private void HandleEvent(object sender, string message)
+  {
+    Console.WriteLine("Event received: " + message);
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    EventPublisher publisher = new EventPublisher();
+    EventSubscriber subscriber = new EventSubscriber(publisher);
+
+    publisher.TriggerEvent("Hello from the event!");
+  }
+}
