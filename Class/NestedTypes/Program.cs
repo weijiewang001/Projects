c7f9c95f6f4817e248841452a9ac1c88559a8257
wayne <weijiewang001@gmail.com>
2025-04-21 22:34:38 +1000

    Nested Types

---
+++ Class/NestedTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class OuterClass
+{
+  private int outerField = 10;
+
+  public class NestedClass
+  {
+    public void DisplayOuterField(OuterClass outer)
+    {
+      Console.WriteLine("Outer field value : " + outer.outerField);
+    }
+  }
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    OuterClass outerObj = new OuterClass();
+    OuterClass.NestedClass nestedObj = new OuterClass.NestedClass();
+
+    nestedObj.DisplayOuterField(outerObj);
+  }
+}
