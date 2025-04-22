c5f232f8edce10e21e3d0b9818208887995e29fb
wayne <weijiewang001@gmail.com>
2025-04-22 21:55:42 +1000

    Inheritance

---
+++ Class/Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Vehicle
+{
+  public string Brand { get; set; }
+
+  public int Year { get; set; }
+
+  public void Start()
+  {
+    Console.WriteLine("The vehicle is started.");
+  }
+
+  public void Stop()
+  {
+    Console.WriteLine("The vehicle is stopped.");
+  }
+}
+
+public class Car : Vehicle
+{
+  public int NumberofDoors { get; set; }
+
+  public void Accelerate()
+  {
+    Console.WriteLine("The car is accelerating.");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Car myCar = new Car();
+
+    myCar.Brand = "Toyota";
+    myCar.Year = 2025;
+
+    myCar.NumberofDoors = 4;
+
+    myCar.Start();
+
+    myCar.Accelerate();
+
+    myCar.Stop();
+
+  }
+}
