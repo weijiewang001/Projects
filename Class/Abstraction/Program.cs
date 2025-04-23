ad83828e4fb567fe7235c0c45e09d9f68eaf1f60
wayne <weijiewang001@gmail.com>
2025-04-24 00:17:51 +1000

    Abstraction

---
+++ Class/Abstraction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public abstract class Shape
+{
+  public abstract double CalculateArea();
+
+  public void Display()
+  {
+    Console.WriteLine("This is a Shape.");
+  }
+}
+
+public class Circle : Shape
+{
+  public double Radius { get; set; }
+
+  public override double CalculateArea()
+  {
+    return Math.PI * Math.Pow(Radius, 2);
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Circle myCircle = new Circle
+    {
+      Radius = 5
+    };
+
+
+    myCircle.Display();
+
+    double area = myCircle.CalculateArea();
+    Console.WriteLine($"This area of the circle is: {area}");
+  }
+
+
+}
