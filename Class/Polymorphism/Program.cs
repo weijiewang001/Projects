e9faf8720b3a60c249f6856c1fe64d37b2ad113d
wayne <weijiewang001@gmail.com>
2025-04-24 22:29:17 +1000

    Polymorphism

---
+++ Class/Polymorphism/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Animal
+{
+  public virtual void MakeSound()
+  {
+    Console.WriteLine("Generic animal sound");
+  }
+}
+
+public class Dog : Animal
+{
+  public override void MakeSound()
+  {
+    Console.WriteLine("Woof!");
+  }
+}
+
+public class Cat : Animal
+{
+  public override void MakeSound()
+  {
+    Console.WriteLine("Meow!");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Animal myDog = new Dog();
+    Animal myCat = new Cat();
+
+    myDog.MakeSound();
+    myCat.MakeSound();
+  }
+}
