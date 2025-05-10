2bce1ae229613b1ba012d8730151d0aaae100366
wayne <weijiewang001@gmail.com>
2025-05-10 18:17:42 +1000

    Challenge

---
+++ ConditionalLogic/Challenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    int Sum = 0;
+    for (int i = 1; i < 21; i++)
+    {
+      if (i % 3 == 0)
+      {
+        Sum += i;
+      }
+    }
+    Console.WriteLine($"Sum is {Sum}");
+  }
+}
