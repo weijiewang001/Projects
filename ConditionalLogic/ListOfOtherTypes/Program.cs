d75dcc707381bf15644bcdd7396ceb84561d57a4
wayne <weijiewang001@gmail.com>
2025-05-10 23:29:20 +1000

    Other type

---
+++ ConditionalLogic/ListOfOtherTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    var fibonacciNumbers = new List<int> { 1, 1 };
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+
+    foreach (var item in fibonacciNumbers)
+    {
+      Console.WriteLine(item);
+    }
+
+  }
+}
