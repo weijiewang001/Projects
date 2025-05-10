8c686608a79507fd675f5dce356ca055a33347ce
wayne <weijiewang001@gmail.com>
2025-05-10 22:39:51 +1000

    Sort example

---
+++ ConditionalLogic/SortList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    List<string> names = new List<string> { "Joe", "Anna", "Phillip", "Maria", "Bill" };
+    var index = names.IndexOf("Maria");
+    if (index != -1)
+    {
+      Console.WriteLine($"The name {names[index]} is at index {index}");
+    }
+    var notFound = names.IndexOf("Not Found");
+    Console.WriteLine($"When an item is not found, InfexOf return {notFound}");
+
+
+    names.Sort();
+    foreach (var name in names)
+    {
+      Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+  }
+}
