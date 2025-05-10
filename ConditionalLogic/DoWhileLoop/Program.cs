83c8f5824a4d6ef9c499c77fbaf05ae0ece2f4e8
wayne <weijiewang001@gmail.com>
2025-05-10 17:48:45 +1000

    Do while

---
+++ ConditionalLogic/DoWhileLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    int counter = 0;
+    do
+    {
+      Console.WriteLine($"Hello World! The counter is {counter}");
+      counter++;
+    } while (counter < 10);
+  }
+}
