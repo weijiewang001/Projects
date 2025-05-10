e80170eff973a8a3e1cc98fad58c1ff7e6574d50
wayne <weijiewang001@gmail.com>
2025-05-10 18:06:15 +1000

    Nest loop

---
+++ ConditionalLogic/NestedLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    for (int row = 1; row < 11; row++)
+    {
+      for (char column = 'a'; column < 'k'; column++)
+      {
+        Console.WriteLine($"The cell is ({row}, {column})");
+      }
+    }
+
+  }
+}
