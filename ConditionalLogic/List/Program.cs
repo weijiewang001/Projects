8e5e26fca3db1cfde23eeb1ad775e3e2f397f5e3
wayne <weijiewang001@gmail.com>
2025-05-10 22:22:37 +1000

    List exercise

---
+++ ConditionalLogic/List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    List<string> names = new List<string> { "Joe", "Jack", "Anna" };
+
+    foreach (var name in names)
+    {
+      Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+
+    names.Add("Maria");
+    names.Add("Bill");
+    names.Remove("Anna");
+
+    foreach (var name in names)
+    {
+      Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+
+    Console.WriteLine($"My name is {names[0]}.");
+    Console.WriteLine($"I have added {names[2]} and {names[3]} to the list");
+    Console.WriteLine($"The list has {names.Count} people in it.");
+
+  }
+}
