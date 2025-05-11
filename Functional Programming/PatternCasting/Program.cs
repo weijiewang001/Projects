9ad1b815f44ce1807a84965ef870c71e99f7a2e3
wayne <weijiewang001@gmail.com>
2025-05-11 22:08:34 +1000

    Casting

---
+++ Functional Programming/PatternCasting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    object obj = 42;
+    int intValue = (int)obj;
+
+    Console.WriteLine($"Casted value: {intValue}");
+  }
+}
