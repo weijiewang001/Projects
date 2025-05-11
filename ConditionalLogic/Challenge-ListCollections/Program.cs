ce9ff1abfd2ec777dac69e34914d7a1f6cc20ddd
wayne <weijiewang001@gmail.com>
2025-05-11 21:32:44 +1000

    Challenge List Collections

---
+++ ConditionalLogic/Challenge-ListCollections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    var fibonacciNumbers = new List<int> { 1, 1 };
+    while (fibonacciNumbers.Count < 20)
+    {
+      fibonacciNumbers.Add(fibonacciNumbers[fibonacciNumbers.Count - 1] + fibonacciNumbers[fibonacciNumbers.Count - 2]);
+
+    }
+
+    foreach (var number in fibonacciNumbers)
+    {
+      Console.WriteLine(number);
+    }
+
+
+  }
+}
