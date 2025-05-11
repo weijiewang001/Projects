9565f0e5ed0a477143915768716e833db353e7b8
wayne <weijiewang001@gmail.com>
2025-05-11 21:44:03 +1000

    Matching Casting

---
+++ Functional Programming/PatternMatchingCasting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    object someObject = 'c';
+    if (someObject is string stringValue)
+    {
+      Console.WriteLine($"The object is a string {stringValue}");
+    }
+    else if (someObject is int intValue)
+    {
+      Console.WriteLine($"The object is a int {intValue}");
+    }
+    else
+    {
+      Console.WriteLine($"The object is of an unknow type");
+    }
+  }
+}
