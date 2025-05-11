7118bd070399cb77779ff54646a836133007bff5
wayne <weijiewang001@gmail.com>
2025-05-11 22:15:33 +1000

    Operator

---
+++ Functional Programming/Operator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    object obj = "Hello";
+    string? strValue = obj as string;
+
+    if (strValue != null)
+    {
+      Console.WriteLine($"Casted Value: {strValue}");
+    }
+    else
+    {
+      Console.Write("Casting failed");
+    }
+  }
+}
