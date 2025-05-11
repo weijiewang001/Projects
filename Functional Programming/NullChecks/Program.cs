3d71b04e109319d138db43896fd40af5d2f77b9c
wayne <weijiewang001@gmail.com>
2025-05-11 22:45:14 +1000

    NullChecks

---
+++ Functional Programming/NullChecks/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    List<string> names = null;
+    if (names != null)
+    {
+      int count = names.Count;
+      Console.WriteLine("Name count is " + count.ToString());
+    }
+    else
+    {
+      Console.WriteLine("Names are null");
+    }
+
+    int countWithNullConditional = names?.Count ?? 0;
+    Console.WriteLine("The null-conditional operator is " + countWithNullConditional.ToString());
+
+
+
+    names = new List<string> { "Alice", "Bob", "Charlie", "David" };
+
+    if (names != null)
+    {
+      int count = names.Count;
+      Console.WriteLine("Name count is " + count.ToString());
+    }
+    else
+    {
+      Console.WriteLine("Names are null");
+    }
+
+    countWithNullConditional = names?.Count ?? 0;
+    Console.WriteLine("The null-conditional operator2 is " + countWithNullConditional.ToString());
+
+  }
+}
