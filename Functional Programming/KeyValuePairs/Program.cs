a6e985ff35c1a759aa273551ce5bf9b80df81214
wayne <weijiewang001@gmail.com>
2025-05-11 22:55:24 +1000

    KeyValuePairs

---
+++ Functional Programming/KeyValuePairs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    var list = new List<KeyValuePair<string, string>> {
+      new KeyValuePair<string, string>("000001", "John"),
+      new KeyValuePair<string, string>("000002", "Joe")
+    };
+
+    list.ForEach(kvp => Console.WriteLine($"{kvp.Key}: {kvp.Value}"));
+  }
+}
