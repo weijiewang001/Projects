9a0e0ad2487be44cbd0079b310d3bb277f3374b2
wayne <weijiewang001@gmail.com>
2025-05-12 23:09:54 +1000

    Custom exception

---
+++ ErrorException/CreatAndThrowException/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+
+public class CustomException : Exception
+{
+  public CustomException(string message) : base(message)
+  {
+
+  }
+
+}
+
+class Program
+{
+  static void Main()
+  {
+    try
+    {
+      throw new CustomException("Custom exception thrown.");
+    }
+    catch (CustomException ex)
+    {
+      Console.WriteLine($"Custom exception occured: {ex.Message}");
+    }
+    finally
+    {
+      Console.WriteLine("Finally block executed.");
+    }
+  }
+}
---
+++ ErrorException/FinallyBlock/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+
+class Program
+{
+  static void Main()
+  {
+    try
+    {
+      // 
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"An error occured: {ex.Message}");
+    }
+    finally
+    {
+      Console.WriteLine("Finally block executed.");
+    }
+  }
+}
