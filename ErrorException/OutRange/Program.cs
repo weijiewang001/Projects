a2aefb522f54c82537ee32950812959263bdc3e1
wayne <weijiewang001@gmail.com>
2025-05-12 22:57:07 +1000

    Exception range

---
+++ ErrorException/OutRange/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+
+class Program
+{
+  static void Main()
+  {
+    try
+    {
+      int[] numbers = { 1, 2, 3 };
+      int index = 4;
+      int value = numbers[index];
+    }
+    catch (IndexOutOfRangeException)
+    {
+      Console.WriteLine($"Index out of range.");
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"An error occured: {ex.Message}");
+    }
+  }
+}
