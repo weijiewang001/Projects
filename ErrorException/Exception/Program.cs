3320aa9073bebea001e1f4fc070e8351c9c92c7a
wayne <weijiewang001@gmail.com>
2025-05-12 22:31:52 +1000

    Try catch

---
+++ ErrorException/Exception/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+
+class Program
+{
+  static void Main()
+  {
+    try
+    {
+      int zeroVal = 0;
+      int result = (10 / zeroVal);
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+  }
+}
