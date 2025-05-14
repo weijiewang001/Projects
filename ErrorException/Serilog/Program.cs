e42b6f7c8576868226ed12d96f2617fcdd4e2ca5
wayne <weijiewang001@gmail.com>
2025-05-14 22:58:43 +1000

    Serilog

---
+++ ErrorException/Serilog/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Serilog;
+
+
+class Program
+{
+  static void Main()
+  {
+    Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+    Log.Information("Hello.Serilog!");
+    Log.Warning("This is a warning message!");
+    Log.Information("An error occurred!");
+
+    Log.CloseAndFlush();
+
+  }
+}
